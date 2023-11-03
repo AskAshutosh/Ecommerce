289fc1496b19972466d002e4eb362633114f145d
AskAshutosh <askashutosh22@gmail.com>
2023-11-03 16:38:39 +0530

    Integrated DB
    Added Repository class for DB Integration, used postgres for now

---
+++ src/main/java/com/ashutosh/EcomProductService/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.ashutosh.EcomProductService.repository;
+
+import com.ashutosh.EcomProductService.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+}
